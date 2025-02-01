9a862bf453a3b69b87ff1552b2e7d18c12e0125c
Audi9602 <audisha9602@gmail.com>
2025-02-01 20:29:26 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my laptop. I am pretty proud of this one cuz my father🌝 and sister🧸 bought it for me. 
+    Recently, I've made a couple of games🎮 in JavaScript. And I like designing stuff too cuz I customize stuff on a whim. 
+    Anyway, regardless, I just like learning new stuff.`,
+
+    degree: `This is my UG degree. I majored in Computer Science Engineering with Health Informatics. 
+    I'm about to graduate in 2025, wish me luck🍀`,
+
+    "sofa-table": `That's my sofa. I like to relax here🥺 and socialise😋 with folks. 
+    I am available on lots of social media sites#️⃣ and up for a small talk anytime. 
+    You can ping me at <a href="https://www.reddit.com/user/addie-infj-wolfed19/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button" 
+    target="_blank">Reddit</a>!`,
+
+    project: `That's my pandora’s box🔮. Filled with random projects🌠. 
+    Maybe it’s not that awesome yet but I’m trying to improve day by day. 
+    I am not even consistent in my efforts to make a new project on regular intervals but I still do it at my pace. 
+    And that’s worth it~🐸. Here’s my <a href="https://github.com/Audi9602" target="_blank">GitHub Repo</a>!`,
+
+    achievements: `I’ll keep it brief. So I have recently co-authored a paper submitted for review in Springer Journal #deep-learning. 
+    And I have published at least 10 articles at GFG. Well that’s me full of variables.😎`,
+
+    certificates: `I have earned certain certifications and badges, you can find it mainly at
+    <a href="https://www.linkedin.com/in/audrija-d-adak-b477a2227/" target="_blank"> LinkedIn </a>☃️.
+    You can always 📞contact me at <a href="mailto:audisha9602@gmail.com">audisha9602@gmail.com</a> if you have any interesting job opportunities!`,
+
+    books: `For me, books📒 are my first escape. I wonder if my house has more bricks🧱 or more books📚 sometimes. 
+    I love books and ebooks but recently I tried audiobooks and now I am growing on them 
+    (Nothing beats music🎵 and podcast🎧 in my list though). 
+    By the way, I used to be an author and published a couple of books on Amazon KDP.`,
+
+    garden: `I’m into gardening🌿 these days. Mainly because my mother🥰 loves gardening. 
+    I found myself a cactus🌵 lately. I can brag about it all day. But not today..`,
+
+    exit: `To exit, you can just close the tab✨.`
+};
